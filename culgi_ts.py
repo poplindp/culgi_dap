f8c725ea0c8e031e1fe352fe3e343619e6a50ae9
dd <dp@vbox>
2018-02-27 10:26:07 +0100

    new version using cvs format, simulations conditions, multiindex option, etc

--- culgi_ts.py
+++ culgi_ts.py
@@ -1,6 +1,301 @@
 #!/usr/bin/python3
+#
+# Functions for ctf df creation
+#
+
+def read_cts(cts_csv, solub=True, intra_corr=True, intra_suffix="_IntraEne",
+        time_step=0.5, nmol=1, debug=True):
+    """function able to read the timeseries file obtained from culgi and
+    correcting, if present, by intramolecular terms
+
+    :cts_csv: CSV culgi time series
+    :solub: add solubility column if possible
+    :intra_corr: if Intramolecular Energy correction is to be done
+    :intra_suffix: suffix of CSV containing intramolecular energies
+    :time_step: simulation time step
+    :nmol: number of polymer molecules (for solubility calculation)
+    :debug: print information
+    :returns: a pandas dataframe containing the time series.
+    """
+
+    import sys
+    import pandas as pd
+
+    #df = pd.read_csv(cts_csv, sep='\t', index_col="Index", parse_dates=True)
+    df = pd.read_csv(cts_csv, index_col="Index", parse_dates=True)
+
+    if solub or intra_corr:
+        cts_intra_csv = cts_csv.replace("_Inst", intra_suffix)  # change name
+        df = intra_energy(df, cts_intra_csv)
+
+    if solub:
+        solubility(df, nmol=nmol)
+
+# rename and rescale index column
+    df.index.rename("Time(ns)", inplace=True)
+    df.index = df.index * time_step
+
+# FIXME: this was useful if internal analysis was done with
+# assign filename value (to keep track where this was coming from)
+    df.filename = cts_csv
 
 
+    return(df)
+
+
+
+def intra_energy(df, cts_intra_csv, debug=True):
+    """ Given a culgi time series df with total energies and a file with intra
+    enegies, modify df to add Inter and Intramolecular Energy cols
+
+    :df: culgi time series dataframe containing total energies
+    :cts_intra_csv: CSV culgi time series containing intra energies
+    :debug: print information
+    :returns: a pandas dataframe containing the time series with intra ener
+    """
+
+    import sys
+    import pandas as pd
+
+    if not any("Intermolecular" in word for word in df.columns):
+
+        try:
+            df_intra = pd.read_csv(cts_intra_csv, index_col="Index",
+                    parse_dates=True)
+        except IOError:
+            if debug:
+                print("Cannot open Intramolecular energy file: '{}'"
+                        .format(cts_intra_csv) , file=sys.stderr)
+            sys.exit(1)
+
+    # adding Intramolecular and units strings
+        df_intra.columns = ['Intramolecular' + str(col) + '(kcal/mol)'
+                for col in df_intra.columns]
+
+    # NOTE: here join forces to return a value since df is a new df, is a copy
+        df = df.join(df_intra)
+
+    # Substracting intramolecular contributions
+
+        el_lbl = "ElectrostaticsEnergy(kcal/mol)"
+        vdw_lbl = "VdWEnergy(kcal/mol)"
+        hb_lbl = "HydrogenbondingEnergy(kcal/mol)"
+        hb_lbl2 = "HydrogenBondingEnergy(kcal/mol)"
+
+        df[el_lbl] = df[el_lbl] - df["Intramolecular"+ el_lbl]
+        df[vdw_lbl] = df[vdw_lbl] - df["Intramolecular"+ vdw_lbl]
+        df[hb_lbl] = df[hb_lbl] - df["Intramolecular" + hb_lbl2]
+
+        if (df["Intramolecular" + hb_lbl2] != 0).any():
+            if debug:
+                print("Intramolecular " + hb_lbl2 + "different to zero!")
+
+        df = df.rename(columns={el_lbl: 'Intermolecular' + el_lbl})
+        df = df.rename(columns={vdw_lbl: 'Intermolecular' + vdw_lbl})
+        df = df.rename(columns={hb_lbl: 'Intermolecular' + hb_lbl})
+
+    return(df)
+
+
+
+def solubility(df, nmol=1, debug=True):
+    """ Given a df containing intramolecular and intermolecular energies
+    calculate and add a new solubility column
+    :df: culgi dataframe containing the energies
+    """
+
+    import sys
+    import pandas as pd
+    import numpy as np
+
+    if any("Solubility" in word for word in df.columns):
+        if debug:
+            print("  Solubility column already present")
+#        return(df)
+
+    n_av = 6.022e23
+    conv_ctte = -1.0 * float(nmol) * 1.0e3 / (n_av * 1e-24)
+
+    prefix_lbl = "Intermolecular"
+    el_lbl = prefix_lbl + 'ElectrostaticsEnergy(kcal/mol)'
+    vdw_label = prefix_lbl + 'VdWEnergy(kcal/mol)'
+    hb_lbl = prefix_lbl + 'HydrogenbondingEnergy(kcal/mol)'
+
+    try:
+        df['Solubility(cal/cc)'] = np.sqrt(conv_ctte *
+                (df[el_lbl] + df[vdw_label] + df[hb_lbl]) /
+                (df['X(A)'] * df['Y(A)'] * df['Z(A)']))
+    except KeyError:
+        print("DataFrame not containing Inter or Intermolecular Energy terms")
+        sys.exit(1)
+
+#   in case to need return a df, instead of calculating df['Solub...'] we could
+#   have used a foo['Solub...'] and do df=df.join(foo['Solub...'])
+#    return(df)
+
+
+def read_culgi_descriptors(cvs_file, singlerow=True):
+    """Takes a csv file containing descriptors for all molecules
+    it returns a dataframe with the mean of all descriptors for everymolecule
+    type "mol_name" (contains filename used by culgi) column is removed and
+    error strings are converted into nan
+    :cvs_file: file containing the timeseries (ctf format)
+    :single_row: returns all descriptors in a single row df
+    :returns: a pandas df containing all descriptors.
+    print("descriptors")
+    """
+
+    import pandas as pd
+    import numpy as np
+
+    # read and remove "mol_name" column
+    df = pd.read_csv(cvs_file).drop("mol_name", axis=1)
+
+    # FIXME: this line is prone error, there is no a good way to determine
+    # which name the polymer is supposed to have.
+    # replace word with "-" usually polymer should contain that
+    df['molecule'] = df['molecule'].replace(r'\b\w+-\w+\b', 'polymer', regex=True)
+
+    # set index to molecule
+    df = df.set_index('molecule')
+
+    # replace string cells (usually errors) into nan
+    df.replace(r'\s+', np.nan, regex=True, inplace=True)
+
+    # convert everything to numeric
+    df = df.apply(pd.to_numeric)
+
+    # caculate mean (separating by index)
+    df = df.mean(level='molecule')
+
+    if singlerow:
+        # fuse rows adding index name to columns
+        df = df.stack(dropna=False).to_frame().T
+        df.columns = ['{}_{}'.format(*c) for c in df.columns]
+
+    return(df)
+
+
+
+#
+# Functions dfmi creation
+#
+
+# FIXME nmol should be read from the input file
+#       in fact input files should be added
+
+def build_dfmi(files="*_Inst.csv", nmol=1, multindex=False, axis=0,
+        verb=False, descrbool=True, condit=True):
+    """function building a multiindex dataframe containing the averages
+    of culgi time series
+    if descriptor files are present, add it to table
+    :files: = name of csv energy files (*Inst.csv avoids problem with
+    IntraEne.csv
+    :nmol: # number of molecules per box  (for solubility calculation)
+    :axis: # if multiindex are in rows (0, uglier but useful to use
+    with .loc or .iloc) or columns (1, just nicer)
+    :condit: includes initial contidions
+    :verb: prints which file is taking
+    :returns: a pandas dataframe multindex containing all <ts> and descript.
+
+    It requires a given directory format:
+        ./system/"runs/"sample/files
+    """
+
+    import pandas as pd
+    import os.path as osp
+    import glob
+    import re
+
+
+    search_path = "**/" + files
+
+    result = pd.DataFrame()
+
+    for fileraw in glob.iglob(search_path, recursive=True):
+
+        lst = []
+
+        # Skip internal energy files
+        if "IntraEne" in fileraw:
+            continue
+
+        file = osp.normcase(osp.normpath(fileraw))
+        if verb:
+            print("Processing:", file)
+
+        # Processing path to extract polymer, system, condition, replica
+        filename = osp.basename(file)
+        dirname = osp.dirname(file)
+        (polsys, rep) = osp.split(dirname)
+        (polymer_full, syscond) = osp.split(polsys)
+        polymer = osp.basename(polymer_full)
+        (system, condition)=syscond.split("-")
+
+        try:
+            replica = int(re.search('.*-(\d+)$', rep)[1])
+        except:
+            print("** Problem parsing replica in:", fileraw)
+            break
+
+
+# add the columns that identify the system
+        ini = pd.DataFrame({'Polymer': polymer, 'Replica': replica,
+            'Filename': filename}, index=[0])
+
+        lst.append(ini)
+
+# add conditions and systems
+
+        if condit:
+            cond = pd.read_csv(dirname+"/data/Conditions.csv")
+            conditions = cond[cond['Condition'].str.contains(condition)]
+            conditions.index = [0] # Need to reset this index to 0
+            lst.append(conditions)
+
+            syst = pd.read_csv(dirname+"/data/Systems.csv")
+            systems = syst[syst['System'].str.contains(system)]
+            systems.index = [0] # Need to reset this index to 0
+            nmol = systems['nChains']
+            lst.append(systems)
+
+        else:
+            ini['Condition'] = condition
+            ini['System'] = system
+
+
+# read energy time series
+        cts_ener = read_cts(fileraw, solub=True, nmol=nmol, intra_corr=True,
+               intra_suffix="_IntraEne", time_step=0.5, debug=True)
+        ener_mean = cts_ener.mean().to_frame().T
+
+        lst.append(ener_mean)
+
+# FIXME: calculate autocorr
+
+# add descriptors
+        fil_descrp = re.sub(r'_Inst', r'_desc', fileraw)
+        fil_exists = osp.exists(fil_descrp)
+        if fil_exists and descrbool:
+            if verb:
+                print("  Descriptors --> " + fil_descrp)
+            cdes = read_culgi_descriptors(fil_descrp)
+            lst.append(cdes)
+
+        if verb:
+            print()
+
+        result = result.append(pd.concat(lst, axis=1))
+
+    if multindex:
+        result = result.set_index(['Polymer', 'System', 'Condition', 'Replica', 'Filename'])
+
+    return(result)
+
+
+
+
+# --- From here older versions
 
 def get_line_number(rawstr, filename):
     """takes a string (maybe a regex) and return the first instance line number
@@ -23,7 +318,7 @@ def get_line_number(rawstr, filename):
 # Functions for ctf df creation
 #
 
-def read_ctf(ctf_file, time_step=0.5):
+def read_ctf_old(ctf_file, time_step=0.5):
     """function able to read the timeseries file obtained from culgi
     :ctf_file: file containing the timeseries (ctf format)
     :time_step: simulation time step
@@ -47,7 +342,7 @@ def read_ctf(ctf_file, time_step=0.5):
     return(ctf)
 
 
-def ctf_intra(ctf, intra_suffix="_IntraEne"):
+def ctf_intra_old(ctf, intra_suffix="_IntraEne"):
     """
     takes a ctf file and a ctf intraenergy file to build and "old format a"
     """
@@ -92,7 +387,7 @@ def ctf_intra(ctf, intra_suffix="_IntraEne"):
     return(ctf)
 
 
-def solubility(ctf, nmol=1, dropna=False):
+def solubility_old(ctf, nmol=1, dropna=False):
     """read the timeseries file obtained from culgi and converts the index
     into time format
     :ctf_file: file containing the timeseries (ctf format)
@@ -103,7 +398,7 @@ def solubility(ctf, nmol=1, dropna=False):
     import pandas as pd
 
     if not any("Intermolecular" in word for word in ctf.columns):
-        print("Intermolecular terms not present. Calculating them.")
+        print("  Intermolecular terms not present. Calculating them.")
         ctf = ctf_intra(ctf)
 
 # FIXME is this convertion really necessary?
@@ -129,7 +424,7 @@ def solubility(ctf, nmol=1, dropna=False):
 
 
 # This function should only be called (mostly) inside build_dfmi
-def read_culgi_descriptors(out_file):
+def read_culgi_descriptors_old(out_file):
     """function able to read the descriptors file obtained from culgi
     :out_file: file containing the timeseries (ctf format)
     :returns: a pandas series containing all descriptors.
@@ -147,14 +442,7 @@ def read_culgi_descriptors(out_file):
     return(df)
 
 
-#
-# Functions dfmi creation
-#
-
-# FIXME nmol should be read from the input file
-#       in fact input files should be added
-
-def build_dfmi(system, sample, files="mm06*.ctf", nmol=1,
+def build_dfmi_OLD(system, sample, files="mm06*.ctf", nmol=1,
                axis=0, verb=False, descrbool=True):
     """function building a multiindex dataframe containing the averages
     of culgi time series
